543fd179574d81bae94ad846d85176786fb8dca7
John Tran <s3929597@rmit.edu.vn>
2025-08-01 13:17:51 +0700

    Change layout/styling/icons and create profile image flip function

---
+++ JS/profileflip.js
@@ -0,0 +1,5 @@
+// This function toggles the 'flipped' class on the profile card element
+function flipCard() {
+  const card = document.getElementById("profileCard");
+  card.classList.toggle("flipped");
+}
